c3bdaf4620e90a71c03f4b3e1134718979cd337f
ido-postelnik <postelnik@gmail.com>
2020-05-01 17:31:36 +0300

    Updates

---
+++ precache-manifest.a7c70aadaf43034643485b37a7a014cc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d76b1d6645389df0d5b6763bb7e45107",
+    "url": "/minesweeper/index.html"
+  },
+  {
+    "revision": "8c3f3b309a50d07c19da",
+    "url": "/minesweeper/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "5d2b7c822f257fc7c9d7",
+    "url": "/minesweeper/static/js/2.6861a02c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/minesweeper/static/js/2.6861a02c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8c3f3b309a50d07c19da",
+    "url": "/minesweeper/static/js/main.d889dd71.chunk.js"
+  },
+  {
+    "revision": "e5190799312cce7549c7",
+    "url": "/minesweeper/static/js/runtime-main.f415ae51.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/minesweeper/static/media/logo.5d5d9eef.svg"
+  }
+]);
