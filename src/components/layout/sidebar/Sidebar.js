48de0ac897783cf13d5ba65bf1ec3d70124235f7
ido-postelnik <postelnik@gmail.com>
2020-05-03 11:11:25 +0300

    [src] Set game images, logo and favicon

--- src/components/layout/sidebar/Sidebar.js
+++ src/components/layout/sidebar/Sidebar.js
@@ -1,14 +1,14 @@
 import React from "react";
 
 import GameSettings from "../../game/GameSettings/GameSettings";
-import LogoImage from "../../../assets/images/minesweeper-logo.png";
+import LogoImage from "../../../assets/images/logo.svg";
 import "./Sidebar.scss";
 
 const Sidebar = () => {
   return (
     <aside className="sidebar">
       <section className="logo p-x-5 p-y-5">
-        <img src={LogoImage} alt="Minesweeper logo" />
+        <img src={LogoImage} alt="Minesweeper logo" className="m-r-5" />
         <h1 className="m-y-0">Minesweeper</h1>
       </section>
       <GameSettings className="game-settings"></GameSettings>
